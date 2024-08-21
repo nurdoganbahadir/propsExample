5edf77d652d7422f4cc9fe91afb74655ec3ff7f0
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-21 22:50:26 +0300

    distructing method added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Dog from "./components/Dog";
 function App() {
   return (
     <div className="App">
-      <Dog name="Siberian Husky" color="gray and white" />
+      <Dog name="Siberian Husky" color="gray" />
     </div>
   );
 }
--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,14 +1,27 @@
-const Dog = (props) => {
+// const Dog = (props) => {
+//   return (
+//     <div>
+//       <p>{props.name}</p>
+//       <img
+//         src="https://www.elityavru.com/images/irk-bilgileri/kopek-irki/siberian-husky-dog.webp"
+//         alt=""
+//       />
+//       <p style={{ backgroundColor: props.color }}>{props.color}</p>
+//     </div>
+//   );
+// };
+
+export default Dog;
+
+const Dog = ({ name, color }) => {
   return (
     <div>
-      <p>{props.name}</p>
+      <p>{name}</p>
       <img
         src="https://www.elityavru.com/images/irk-bilgileri/kopek-irki/siberian-husky-dog.webp"
         alt=""
       />
-      <p>{props.color}</p>
+      <p style={{ backgroundColor: color }}>{color}</p>
     </div>
   );
 };
-
-export default Dog;
