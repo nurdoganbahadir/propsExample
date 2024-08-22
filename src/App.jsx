805bd4a2eba2f0e7ef2d9870331e3b0806e2eeba
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-22 16:45:59 +0300

    arttir ve azalt fonksiyonları app içine eklendi displayde gösterildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import Dog from "./components/Dog";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const arttir = () => {
+    setCount(count+1);
+  };
+  const azalt = () => {
+    if (count > 0) {
+      setCount(count-1);
+    }
+  };
+
   return (
     <div className="App">
       <Dog
@@ -9,7 +20,16 @@ function App() {
         color="gray"
         img="https://www.elityavru.com/images/irk-bilgileri/kopek-irki/siberian-husky-dog.webp"
       />
-      <Dog name="Sivas Kangalı" color="white" img="https://www.sivasekspres.com/files/uploads/news/default/n-a-729005.webp" />
+      <Dog
+        name="Sivas Kangalı"
+        color="white"
+        img="https://www.sivasekspres.com/files/uploads/news/default/n-a-729005.webp"
+      />
+      <div>
+        <p>{count}</p>
+        <button onClick={arttir}>Arttır</button>
+        <button onClick={azalt}>Azalt</button>
+      </div>
     </div>
   );
 }
